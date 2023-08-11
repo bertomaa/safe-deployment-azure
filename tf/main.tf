0cd417769926b7015006d8380b4d2d19fd766a97
Marco Bertolino <marcobertolino.developer@gmail.com>
2023-08-11 14:23:50 +0200

    added function app

--- tf/main.tf
+++ tf/main.tf
@@ -20,10 +20,34 @@ resource "azurerm_storage_container" "web_content" {
   container_access_type = "private"
 }
 
-resource "azurerm_service_plan" "safe_deploy_function_plan" {
-  name                = "safe-deploy-functions${var.environment}"
+resource "azurerm_service_plan" "safe_deploy_service_plan" {
+  name                = "sd-service-plan-${var.environment}"
   location            = data.azurerm_resource_group.safe-deploy-rg.location
   resource_group_name = data.azurerm_resource_group.safe-deploy-rg.name
   os_type                    = "Linux"
   sku_name = "Y1"
+}
+
+resource "azurerm_linux_function_app" "visitors-app" {
+  name                = "safe-deploy-function-app-${var.environment}"
+  resource_group_name = data.azurerm_resource_group.safe-deploy-rg.name
+  location            = data.azurerm_resource_group.safe-deploy-rg.location
+
+  storage_account_name       = azurerm_storage_account.safe_deploy_storage.name
+  storage_account_access_key = azurerm_storage_account.safe_deploy_storage.primary_access_key
+  service_plan_id            = azurerm_service_plan.safe_deploy_service_plan.id
+
+  https_only      = true
+  #zip_deploy_file = "../../api/visitors-functions.zip"
+  site_config {
+    application_stack {
+      node_version = 18.0
+    }
+  }
+
+  # app_settings = {
+  #   WEBSITE_RUN_FROM_PACKAGE=1
+  # }
+
+  #APP SETTINGS -> ENVIRONMENT VARIABLES TO ACCESS COSMOS DB
 }
