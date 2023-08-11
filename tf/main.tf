c060e7d504226f1387a0806440318ebcc2fd6fe8
Marco Bertolino <marcobertolino.developer@gmail.com>
2023-08-11 13:29:42 +0200

    other resources use improted rg

--- tf/main.tf
+++ tf/main.tf
@@ -8,8 +8,8 @@ data "azurerm_resource_group" "safe-deploy-rg" {
 
 resource "azurerm_storage_account" "safe_deploy_storage" {
   name                     = "safedeploystorage"
-  resource_group_name      = azurerm_resource_group.safe-deploy-rg.name
-  location                 = azurerm_resource_group.safe-deploy-rg.location
+  resource_group_name      = data.azurerm_resource_group.safe-deploy-rg.name
+  location                 = data.azurerm_resource_group.safe-deploy-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -22,8 +22,8 @@ resource "azurerm_storage_container" "web_content" {
 
 resource "azurerm_service_plan" "safe_deploy_function_plan" {
   name                = "safe-deploy-function-plan"
-  location            = azurerm_resource_group.safe-deploy-rg.location
-  resource_group_name = azurerm_resource_group.safe-deploy-rg.name
+  location            = data.azurerm_resource_group.safe-deploy-rg.location
+  resource_group_name = data.azurerm_resource_group.safe-deploy-rg.name
   os_type                    = "Linux"
   sku_name = "Y1"
 }
